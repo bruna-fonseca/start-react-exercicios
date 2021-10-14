7ec7b37aaa6698611fe56772f54eef82b32ec53c
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-10-14 12:29:33 -0300

    atividade mensagem

---
+++ exercicio-mensagem/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Message from './components/Message';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Mensagem de esperança</h1>
+     <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ exercicio-mensagem/src/components/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Message() {
+  return (
+    <div>
+        <pre>
+          Se a caminhada está difícil, é porque você está no caminho certo.
+        </pre>
+    </div>
+  );
+};
